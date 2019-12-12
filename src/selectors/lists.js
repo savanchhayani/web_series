a8116be296c7378145d23cea11b3fc2c93f30dc2
Savan Chhayani <savanchhayani@gmail.com>
2019-12-12 23:21:50 +0530

    added comments

--- src/selectors/lists.js
+++ src/selectors/lists.js
@@ -1,18 +1,26 @@
 import { createSelector } from 'reselect';
 
 const getLists = state => state.lists.lists;
-const getSearchedSeries = state => state.lists.searchedSeriesText;
+const getSearchedSeriesText = state => state.lists.searchedSeriesText;
 
+/**
+ * filters the web series with the searched text.
+ * if search text is empty than returns all web series.
+ * else returns the list of the matched serch text web series.
+ */
 const getFilteredLists = createSelector(
-  [getLists, getSearchedSeries],
+  [getLists, getSearchedSeriesText],
   (lists, searchedSeriesText) => {
     if (searchedSeriesText.trim() === '') { return lists; }
     return lists.filter(item => item.name.trim().includes(searchedSeriesText.trim()));
   },
 );
 
+/**
+ * @returns {String} - the no web series found string text.
+ */
 const getNoSearchResultMessage = createSelector(
-  [getFilteredLists, getSearchedSeries],
+  [getFilteredLists, getSearchedSeriesText],
   (lists, searchedSeriesText) => {
     if (searchedSeriesText.trim() !== '' && lists.length === 0) {
       return `No web series found match with ${searchedSeriesText}`;
@@ -23,6 +31,6 @@ const getNoSearchResultMessage = createSelector(
 
 export {
   getFilteredLists,
-  getSearchedSeries,
+  getSearchedSeriesText as getSearchedSeries,
   getNoSearchResultMessage,
 }
